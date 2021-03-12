637af3063504b6b8a4fad0a7f17292ad706b1a9e
xhrist14n <xhrist14n@gmail.com>
2021-03-11 23:46:26 -0500

    app app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
+  <HelloWorld msg="Hola Mundo de Prueba" />
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import Vue from 'vue'
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import './styles/app.scss'
+
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 createApp(App).mount('#app')
